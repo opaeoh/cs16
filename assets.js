01a472853dbe33ee575e605bdc9ba3277ef0e3f3
Opaeoh <opae.ash@gmail.com>
2025-03-08 17:39:26 +0530

    assets.js

---
+++ assets.js
@@ -0,0 +1,506 @@
+const assets = [
+    {
+        id: 1,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/Wh1tepill-gun.png?v=1738383508452",
+        name: "Wh1tepill-gun",
+        type: "guns",
+        price: 497
+    },
+    {
+        id: 2,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/Wh1tepill-spray.png?v=1738383519374",
+        name: "Wh1tepill-spray",
+        type: "sprays",
+        price: 497
+    },
+    {
+        id: 3,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/Wh1tepill-knife.png?v=1738383526631",
+        name: "Wh1tepill-knife",
+        type: "knives",
+        price: 497
+    },
+    {
+        id: 4,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/techniacus-knife.png?v=1738386553731",
+        name: "Techniacus-knife",
+        type: "knives",
+        price: 371
+    },
+    {
+        id: 5,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/techniacus-spray.png?v=1738386551449",
+        name: "Techniacus-spray",
+        type: "sprays",
+        price: 371
+    },
+    {
+        id: 6,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/techniacus-gun.png?v=1738386547987",
+        name: "Techniacus-gun",
+        type: "guns",
+        price: 371
+    },
+    {
+        id: 7,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/opaeoh-spray.png?v=1738386541987",
+        name: "Opaeoh-spray",
+        type: "sprays",
+        price: 238
+    },
+    {
+        id: 8,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/opaeoh-knife.png?v=1738386538929",
+        name: "Opaeoh-knife",
+        type: "knives",
+        price: 238
+    },
+    {
+        id: 9,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/opaeoh-gun.png?v=1738386536893",
+        name: "Opaeoh-gun",
+        type: "guns",
+        price: 238
+    },
+    {
+        id: 10,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/WagieWeeb-knife.png?v=1738386528131",
+        name: "WagieWeeb-knife",
+        type: "knives",
+        price: 189
+    },
+    {
+        id: 11,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/WagieWeeb-spray.png?v=1738386527263",
+        name: "WagieWeeb-spray",
+        type: "sprays",
+        price: 189
+    },
+    {
+        id: 12,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/WagieWeeb-gun.png?v=1738386526513",
+        name: "WagieWeeb-gun",
+        type: "guns",
+        price: 189
+    },
+    {
+        id: 13,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/MLpropulsion-knife.png?v=1738386513568",
+        name: "MLpropulsion-knife",
+        type: "knives",
+        price: 235
+    },
+    {
+        id: 14,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/MLpropulsion-spray.png?v=1738386509951",
+        name: "MLpropulsion-spray",
+        type: "sprays",
+        price: 235
+    },
+    {
+        id: 15,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/MLpropulsion-gun.png?v=1738386507924",
+        name: "MLpropulsion-gun",
+        type: "guns",
+        price: 235
+    },
+    {
+        id: 16,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/calbch-gun.png?v=1738386485127",
+        name: "calbch-gun",
+        type: "guns",
+        price: 301
+    },
+    {
+        id: 17,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/calbch-knife.png?v=1738386491814",
+        name: "calbch-knife",
+        type: "knives",
+        price: 301
+    },
+    {
+        id: 18,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/calbch-spray.png?v=1738386491326",
+        name: "calbch-spray",
+        type: "sprays",
+        price: 301
+    },
+    {
+        id: 19,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/NewAgeRetroNerd-gun.png?v=1738386472662",
+        name: "NewAgeRetroNerd-gun",
+        type: "guns",
+        price: 189
+    },
+    {
+        id: 20,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/NewAgeRetroNerd-knife.png?v=1738386478202",
+        name: "NewAgeRetroNerd-knife",
+        type: "knives",
+        price: 189
+    },
+    {
+        id: 21,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/NewAgeRetroNerd-spray.png?v=1738386477761",
+        name: "NewAgeRetroNerd-spray",
+        type: "sprays",
+        price: 189
+    },
+    {
+        id: 22,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/donsatamoto-gun.png?v=1738386455616",
+        name: "donsatamoto-gun",
+        type: "guns",
+        price: 319
+    },
+    {
+        id: 23,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/donsatamoto-knife.png?v=1738386459546",
+        name: "donsatamoto-knife",
+        type: "knives",
+        price: 319
+    },
+    {
+        id: 24,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/donsatamoto-spray.png?v=1738386457593",
+        name: "donsatamoto-spray",
+        type: "sprays",
+        price: 319
+    },
+    {
+        id: 25,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/kianerfaan-knife.png?v=1738386414701",
+        name: "kianerfaan-knife",
+        type: "knives",
+        price: 203
+    },
+    {
+        id: 26,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/kianerfaan-gun.png?v=1738386414488",
+        name: "kianerfaan-gun",
+        type: "guns",
+        price: 203
+    },
+    {
+        id: 27,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/kianerfaan-spray.png?v=1738386413083",
+        name: "kianerfaan-spray",
+        type: "sprays",
+        price: 203
+    },
+    {
+        id: 28,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/prthgo-spray.png?v=1738386416402",
+        name: "prthgo-spray",
+        type: "sprays",
+        price: 459
+    },
+    {
+        id: 29,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/prthgo-knife.png?v=1738386413743",
+        name: "prthgo-knife",
+        type: "knives",
+        price: 459
+    },
+    {
+        id: 30,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/prthgo-gun.png?v=1738386411358",
+        name: "prthgo-gun",
+        type: "guns",
+        price: 459
+    },
+    {
+        id: 31,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/TechMemeKing-knife.png?v=1738386379519",
+        name: "TechMemeKing-knife",
+        type: "knives",
+        price: 216
+    },
+    {
+        id: 32,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/TechMemeKing-spray.png?v=1738386378169",
+        name: "TechMemeKing-spray",
+        type: "sprays",
+        price: 216
+    },
+    {
+        id: 33,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/TechMemeKing-gun.png?v=1738386373424",
+        name: "TechMemeKing-gun",
+        type: "guns",
+        price: 216
+    },
+    {
+        id: 34,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/shinbehaviour-knife.png?v=1738386358062",
+        name: "shinbehaviour-knife",
+        type: "knives",
+        price: 315
+    },
+    {
+        id: 35,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/shinbehaviour-spray.png?v=1738386356309",
+        name: "shinbehaviour-spray",
+        type: "sprays",
+        price: 315
+    },
+    {
+        id: 36,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/shinbehaviour-gun.png?v=1738386353589",
+        name: "shinbehaviour-gun",
+        type: "guns",
+        price: 315
+    },
+    {
+        id: 37,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/BasedNorthmathr-knife.png?v=1738386339605",
+        name: "BasedNorthmathr-knife",
+        type: "knives",
+        price: 245
+    },
+    {
+        id: 38,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/BasedNorthmathr-spray.png?v=1738386338244",
+        name: "BasedNorthmathr-spray",
+        type: "sprays",
+        price: 245
+    },
+    {
+        id: 39,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/BasedNorthmathr-gun.png?v=1738386334518",
+        name: "BasedNorthmathr-gun",
+        type: "guns",
+        price: 245
+    },
+    {
+        id: 40,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/djcows-knife.png?v=1738386314217",
+        name: "djcows-knife",
+        type: "knives",
+        price: 279
+    },
+    {
+        id: 41,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/djcows-spray.png?v=1738386303267",
+        name: "djcows-spray",
+        type: "sprays",
+        price: 279
+    },
+    {
+        id: 42,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/djcows-gun.png?v=1738386299105",
+        name: "djcows-gun",
+        type: "guns",
+        price: 279
+    },
+    {
+        id: 43,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/_kiyosh1-gun.png?v=1738385409522",
+        name: "_kiyosh1-gun",
+        type: "guns",
+        price: 200
+    },
+    {
+        id: 44,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/_kiyosh1-knife.png?v=1738385408323",
+        name: "_kiyosh1-knife",
+        type: "knives",
+        price: 200
+    },
+    {
+        id: 45,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/_kiyosh1-spray.png?v=1738385407222",
+        name: "_kiyosh1-spray",
+        type: "sprays",
+        price: 200
+    },
+    {
+        id: 46,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/major_kasturAGI-knife.png?v=1738385365668",
+        name: "major_kasturAGI-knife",
+        type: "knives",
+        price: 314
+    },
+    {
+        id: 47,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/major_kasturAGI-spray.png?v=1738385362829",
+        name: "major_kasturAGI-spray",
+        type: "sprays",
+        price: 314
+    },
+    {
+        id: 48,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/major_kasturAGI-gun.png?v=1738385361091",
+        name: "major_kasturAGI-gun",
+        type: "guns",
+        price: 314
+    },
+    {
+        id: 49,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/proffleon-knife.png?v=1738385347722",
+        name: "proffleon-knife",
+        type: "knives",
+        price: 380
+    },
+    {
+        id: 50,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/proffleon-spray.png?v=1738385345409",
+        name: "proffleon-spray",
+        type: "sprays",
+        price: 380
+    },
+    {
+        id: 51,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/proffleon-gun.png?v=1738385342032",
+        name: "proffleon-gun",
+        type: "guns",
+        price: 380
+    },
+    {
+        id: 52,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/remstack-knife.png?v=1738385333113",
+        name: "remstack-knife",
+        type: "knives",
+        price: 247
+    },
+    {
+        id: 53,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/remstack-spray.png?v=1738385329723",
+        name: "remstack-spray",
+        type: "sprays",
+        price: 247
+    },
+    {
+        id: 54,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/remstack-gun.png?v=1738385327921",
+        name: "remstack-gun",
+        type: "guns",
+        price: 247
+    },
+    {
+        id: 55,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/dotproductt-knife.png?v=1738383825235",
+        name: "dotproductt-knife",
+        type: "knives",
+        price: 429
+    },
+    {
+        id: 56,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/dotproductt-spray.png?v=1738383824037",
+        name: "dotproductt-spray",
+        type: "sprays",
+        price: 429
+    },
+    {
+        id: 57,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/dotproductt-gun.png?v=1738383822555",
+        name: "dotproductt-gun",
+        type: "guns",
+        price: 429
+    },
+    {
+        id: 58,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/SpaceSandT-knife.png?v=1738383759388",
+        name: "SpaceSandT-knife",
+        type: "knives",
+        price: 190
+    },
+    {
+        id: 59,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/SpaceSandT-spray.png?v=1738383755518",
+        name: "SpaceSandT-spray",
+        type: "sprays",
+        price: 190
+    },
+    {
+        id: 60,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/SpaceSandT-gun.png?v=1738383753164",
+        name: "SpaceSandT-gun",
+        type: "guns",
+        price: 190
+    },
+    {
+        id: 61,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/ThuleanFuturist-knife.png?v=1738383735864",
+        name: "ThuleanFuturist-knife",
+        type: "knives",
+        price: 410
+    },
+    {
+        id: 62,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/ThuleanFuturist-spray.png?v=1738383732679",
+        name: "ThuleanFuturist-spray",
+        type: "sprays",
+        price: 410
+    },
+    {
+        id: 63,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/ThuleanFuturist-gun.png?v=1738383728968",
+        name: "ThuleanFuturist-gun",
+        type: "guns",
+        price: 410
+    },
+    {
+        id: 64,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/ThatsAllFrens-spray.png?v=1738383718893",
+        name: "ThatsAllFrens-spray",
+        type: "sprays",
+        price: 344
+    },
+    {
+        id: 65,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/ThatsAllFrens-gun.png?v=1738383716610",
+        name: "ThatsAllFrens-gun",
+        type: "guns",
+        price: 344
+    },
+    {
+        id: 66,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/ThatsAllFrens-knife.png?v=1738383722987",
+        name: "ThatsAllFrens-knife",
+        type: "knives",
+        price: 344
+    },
+    {
+        id: 67,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/Sunsettler-knife.png?v=1738383703513",
+        name: "Sunsettler-knife",
+        type: "knives",
+        price: 242
+    },
+    {
+        id: 68,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/Sunsettler-spray.png?v=1738383701809",
+        name: "Sunsettler-spray",
+        type: "sprays",
+        price: 242
+    },
+    {
+        id: 69,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/Sunsettler-gun.png?v=1738383696876",
+        name: "Sunsettler-gun",
+        type: "guns",
+        price: 242
+    },
+    {
+        id: 70,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/dobjormin-knife.png?v=1738383684266",
+        name: "dobjormin-knife",
+        type: "knives",
+        price: 175
+    },
+    {
+        id: 71,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/dobjormin-spray.png?v=1738383681179",
+        name: "dobjormin-spray",
+        type: "sprays",
+        price: 175
+    },
+    {
+        id: 72,
+        image: "https://cdn.glitch.global/c0e1e6d6-9307-4dd8-853d-9853ec14ddee/dobjormin-gun.png?v=1738383675543",
+        name: "dobjormin-gun",
+        type: "guns",
+        price: 175
+    }
+];
